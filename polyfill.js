46520b607a2387e753cd917dcc9e642bf8686f5f
alvinleung1996 <alvinleung1996@outlook.com>
2017-07-11 17:37:46 +0800

    Added image viewer scene
    Added image viewer scene.

--- polyfill.js
+++ polyfill.js
@@ -1,5 +1,5 @@
 /*
- * Number.isInteger
+ * Number.isInteger()
  */
 Number.isInteger = Number.isInteger || function(value) {
   return typeof value === 'number' && 
@@ -19,3 +19,12 @@ Number.isInteger = Number.isInteger || function(value) {
 //   });
 //   window.addEventListener("test", null, options);
 // } catch (e) {}
+
+/*
+ * String.prototype.startsWith()
+ */
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      return this.substr(position || 0, searchString.length) === searchString;
+  };
+}
